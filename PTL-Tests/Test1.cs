c02a956b0230cd13160d346034d2f9033c609e24
EmmaBlanchoud <156648946+EmmaBl9@users.noreply.github.com>
2025-10-28 23:44:05 +0100

    feat: mise en place du projet de test et ajout d'un test

---
+++ PTL-Tests/Test1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using plot_those_lines.Services;
+
+namespace PTL_Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void SetDateRange_DevraitDefinirLesAnnees()
+        {
+            // Arrange
+            var service = new DateFilter();
+
+            // Act
+            service.SetDateRange(2015, 2020);
+
+            // Assert
+            Assert.AreEqual(2015, service.StartYear);
+            Assert.AreEqual(2020, service.EndYear);
+        }
+
+    }
+}
