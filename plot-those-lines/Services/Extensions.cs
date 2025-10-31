83fbecf4953f173cadf94504609567d59ef2ca81
EmmaBlanchoud <156648946+EmmaBl9@users.noreply.github.com>
2025-10-31 15:11:49 +0100

    feat: ajout des deux extensions et utilisation

---
+++ plot-those-lines/Services/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace plot_those_lines.Services
+{
+    // Extensions 1 permettant d'arrondir un nombre decimal
+    public static class DecimalExtensions
+    {
+        public static decimal? RoundTo2(this decimal? value)
+        {
+            return value.HasValue ? Math.Round(value.Value, 2) : null;
+        }
+    }
+
+    //Extensions 2 permettant de vérifier si la liste est vide
+    public static class ListExtensions
+    {
+        public static bool IsEmpty<T>(this List<T> list)
+        {
+            return list == null || list.Count == 0;
+        }
+    }
+}
